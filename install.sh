580336ce7e7794e87e1cb127eec827bbe7c925b2
Chris Holland <zrenfire@gmail.com>
2016-02-28 17:07:39 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+### Everyone
+sudo apt-get install htop
+sudo apt-get install redshift redshift-gtk
+
+### Gaming
+sudo apt-get install steam
+
+### Developers
+sudo apt-get install git
+sudo apt-get install sqlitebrowser
+
+# http://askubuntu.com/questions/172698/how-do-i-install-sublime-text-2-3
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get install sublime-text-installer
+
+### User
+sudo apt-get update livestreamer
