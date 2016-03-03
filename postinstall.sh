94237f65b9ad6b27a0d391a6d9a95c61b312d1d2
Chris Holland <zrenfire@gmail.com>
2016-03-02 23:22:43 -0500

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -9,7 +9,8 @@ sudo apt-get install -y vlc
 
 
 ### Gaming
-sudo apt-get install -y steam
+sudo apt-get install -y steam # Prompts license agreement. TODO: Send <tab>Enter
+# TODO: Run steam to run updater gui
 
 
 
@@ -20,7 +21,7 @@ sudo apt-get install -y pgadmin3
 sudo apt-get install -y postgresql postgresql-contrib
 sudo apt-get install -y python-pip
 sudo apt-get install -y sqlitebrowser
-sudo apt-get install -y wireshark
+sudo apt-get install -y wireshark # Prompt to create wireshark user group (default: no). TODO: Send Enter
 
 ## NodeJS
 # https://nodejs.org/en/download/package-manager/
@@ -34,7 +35,7 @@ sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
 sudo apt-get install -y sublime-text-installer
 # https://packagecontrol.io/installation
-wget https://packagecontrol.io/Package%20Control.sublime-package -P ~/.config/sublime-text-3/Installed Packages/
+wget https://packagecontrol.io/Package%20Control.sublime-package -P "~/.config/sublime-text-3/Installed Packages/"
 
 
 
@@ -43,12 +44,10 @@ sudo apt-get install -y livestreamer
 sudo apt-get install -y inotify-tools
 
 ## NodeJS Packages
-npm install -g ionic@beta
 
 
 
 ### Desktop
-gsettings set org.mate.desktop.applications.browser exec chrome-browser
-gsettings set org.mate.caja.preferences show-hidden-files true
+
 
 
